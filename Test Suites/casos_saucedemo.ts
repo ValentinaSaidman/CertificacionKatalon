4f3e29ccfe807071e5e5a28d4bf0a06c0d7a1d5b
VSaidman <vsaidman@ecosistemas.com.ar>
2024-02-07 17:53:59 -0300

    Certificación Katalon

---
+++ Test Suites/casos_saucedemo.ts
@@ -0,0 +1,42 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>casos_saucedemo</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>3df5df9c-0fd9-4f60-803a-b87c2b609501</testSuiteGuid>
+   <testCaseLink>
+      <guid>bb0a67cd-2bea-4f57-801a-6fd92972e9b7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Saucedemo_inicio_sesion/CP1_inicio_sesion_usuarioValido1</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>5875985d-e3e1-478b-a5f6-674f7639caef</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Saucedemo_inicio_sesion/CP11_inicio_sesion_usuarioValido3</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b7930e68-011c-456a-8460-4b213e24fb85</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Saucedemo_inicio_sesion/CP7_inicio_sesion_usuario_invalido</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>9dfcb071-3f1e-4dd1-b3a5-2cfd8c622c70</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Saucedemo_inicio_sesion/CP6_inicio_sesion_contraseña_invalida</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
